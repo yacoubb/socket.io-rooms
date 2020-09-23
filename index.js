546540fa8b54b6642a40d591cb446176d2508bfb
Yacoub Ahmed <yacoub.ahmedy@gmail.com>
2020-09-23 21:24:33 +0100

    File restructure
    Added src folder.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const clientFactory = require('./client');
-const toRoomServer = require('./server');
-const codes = require('./codes');
+const clientFactory = require('./src/client');
+const toRoomServer = require('./src/server');
+const codes = require('./src/codes');
 
 module.exports = {
 	clientFactory,
